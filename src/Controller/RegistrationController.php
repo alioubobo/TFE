4051922818c6a15073a97e0736e9da7ea8c54dea
alioubobo <alioubobo.abdoutlook.be>
2023-05-14 17:28:56 +0200

    Configuration du reinitialisation du mot de passe

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -62,13 +62,15 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@ars.com', 'ARS'))
+                    ->from(new Address(
+                        $this->getParameter('app.mail_from_address'),
+                        $this->getParameter('app.mail_from_name'),
+                    ))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
-
+           
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
