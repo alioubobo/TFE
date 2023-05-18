1755eb96ad40162b950bccbf23cdd7901c270ce4
alioubobo <alioubobo.abdoutlook.be>
2023-05-18 19:30:26 +0200

    Mis à jour du banner et la page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('contact/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+
+            $address = $data['email'];
+            $content = $data['content'];
+
+            $email = (new Email())
+            ->from($address)              
+            ->to($this->getParameter('app.mail_from_address'))
+            ->subject('Prise de contact')
+            ->text($content);
+
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+        }
+
+        return $this->renderForm('contact/contact.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
